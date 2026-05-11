645afc87f149d044b5cb8e3fcd8769478fecfc49
Samuel Viana da Silva <samuelviana2626@gmail.com>
2026-05-10 23:23:44 -0400

    feat: docker zoado

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,11 +3,15 @@ set -e
 
 mkdir -p /app/staticfiles
 
-chown -R app:app /app/staticfiles
-chmod -R 775 /app/staticfiles
+chmod -R 777 /app/staticfiles || true
+rm -rf /app/staticfiles/* || true
+chmod -R 777 /app/staticfiles || true
 
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-python manage.py compilemessages
+python manage.py collectstatic --noinput --clear
+python manage.py compilemessages || true
+
+chown -R app:app /app/staticfiles || true
+chmod -R 775 /app/staticfiles || true
 
 exec su-exec app "$@"
